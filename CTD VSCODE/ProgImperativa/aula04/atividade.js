ffdbab9f141799c6741fd332d60b61d6acb22926
TatiAlmeida88 <89025314+TatiAlmeida88@users.noreply.github.com>
2021-08-18 19:30:18 -0300

    Primeiro commit

---
+++ CTD VSCODE/ProgImperativa/aula04/atividade.js
@@ -0,0 +1,7 @@
+var nome = prompt('Qual o seu nome?')
+
+var dolar = prompt('Quantos dólares você quer cotar?')
+
+var resultado = dolar*5.60
+
+alert('Olá' + ' ' + nome + ', U$ ' + dolar + ' valem R$ ' + resultado + ' hoje.')
