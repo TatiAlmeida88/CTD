efe49db053f572357aee2cad66716ff4fbecd657
TatiAlmeida88 <89025314+TatiAlmeida88@users.noreply.github.com>
2021-08-23 21:06:50 -0300

    Add files via upload

---
+++ ProgImperativa/aula04/script.js
@@ -0,0 +1,11 @@
+// Comentário de uma única linha.
+
+/*
+    Comentário de múltiplas linhas.
+    Utilizada principalmente em documentações de código.
+*/
+
+var saudacao = 'Olá'
+var nome = prompt('Qual o seu nome?')
+
+alert (saudacao + ' ' + nome)
